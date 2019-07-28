370a497e14cae795552eb37d02e70968eca63f0c
Bianca Stöckl <bianca@artofbibi.de>
2019-07-28 16:59:51 +0200

    schmarrn gmacht

--- models/Question.js
+++ models/Question.js
@@ -4,7 +4,10 @@ const Schema = mongoose.Schema;
 const questionSchema = new Schema({
   // ObjectId: String,
   question: String,
-  category: { type: String, required: true }
+  peerQuestion: String,
+  category: { type: String, required: true },
+  prompt1: String,
+  prompt2: String
 });
 
 const Question = mongoose.model("Question", questionSchema);
