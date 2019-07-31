ea28b1bd3553b17ca8ff7f5df02855ee29ae7e2e
seabysalt <seabysalt@gmail.com>
2019-07-31 23:54:53 +0200

    updated mongoDB for Heroku

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ require("./configs/passport");
 
 
 mongoose
-  .connect(process.env.MONGODB_URI || "mongodb://localhost/project3" || "mongodb://heroku_qhzj8zgv:6sbn1tcr5no0icklt65kaha7qa@ds353957.mlab.com:53957/heroku_qhzj8zgv" , {
+  .connect(process.env.MONGODB_URI || "mongodb://heroku_qhzj8zgv:6sbn1tcr5no0icklt65kaha7qa@ds353957.mlab.com:53957/heroku_qhzj8zgv" , {
     useNewUrlParser: true
   })
   .then(x => {
